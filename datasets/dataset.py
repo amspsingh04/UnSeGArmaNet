e1ae58fe96aab3611e344d526831381d9e9efebf
Shourya Pratap Singh <93045361+amspsingh04@users.noreply.github.com>
2025-08-07 12:44:57 +0530

    Added custom support

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -6,9 +6,10 @@ from tqdm import tqdm
 
 class Dataset:
     def __init__(self, dataset):
-        if dataset not in ["CUB", "ECSSD", "DUTS"]:
+        if dataset not in ["CUB", "ECSSD", "DUTS", "CUSTOM"]:
             raise ValueError(f'Dataset: {dataset} is not supported')
         self.dataset = dataset
+
         if dataset == "CUB":
             self.images, self.masks = load_cub()
         elif dataset == "ECSSD":
@@ -17,6 +18,8 @@ class Dataset:
             self.masks = ds["masks"]
         elif dataset == "DUTS":
             self.images, self.masks = load_duts()
+        elif dataset == "CUSTOM":
+            self.images, self.masks = load_custom()
         self.loader = len(self.images)
 
     def load_samples(self):
@@ -25,38 +28,46 @@ class Dataset:
                 if self.dataset == "CUB":
                     img = np.asarray(Image.open(imagep))
                     seg = np.asarray(Image.open(true_maskp).convert('L'))
-                    true_mask = np.where(seg >= 200,1,0)
+                    true_mask = np.where(seg >= 200, 1, 0)
+
                 elif self.dataset == "ECSSD":
                     img = np.asarray(imagep)
                     seg = np.asarray(true_maskp)
                     true_mask = np.where(seg == True, 1, 0)
-                if self.dataset == "DUTS":
+
+                elif self.dataset == "DUTS":
                     img = np.asarray(Image.open(imagep))
                     seg = np.asarray(Image.open(true_maskp).convert('L'))
-                    true_mask = np.where(seg == 255,1,0).astype(np.uint8)
+                    true_mask = np.where(seg == 255, 1, 0).astype(np.uint8)
+
+                elif self.dataset == "CUSTOM":
+                    img = np.asarray(Image.open(imagep).convert('RGB'))
+                    seg = np.asarray(Image.open(true_maskp).convert('L'))
+                    true_mask = np.where(seg > 127, 1, 0).astype(np.uint8)
+
                 yield img, true_mask
             except Exception as e:
-                print(e)
+                print(f"Error loading {imagep} or {true_maskp}: {e}")
             finally:
                 self.loader -= 1
 
-           
+
 def load_cub():
     cp = os.path.join(os.getcwd(), 'datasets')
-    
+
     fold = f'{cp}/segmentations'
     file_paths = []
     for root, _, files in os.walk(fold):
         for file in files:
-            file_paths.append(os.path.join(root,file))
+            file_paths.append(os.path.join(root, file))
 
     fold2 = f'{cp}/CUB_200_2011/images'
     fp2 = []
     for root, _, files in os.walk(fold2):
         for file in files:
-            fp2.append(os.path.join(root,file))
+            fp2.append(os.path.join(root, file))
 
-    fp2  = sorted(fp2)
+    fp2 = sorted(fp2)
     file_paths = sorted(file_paths)
 
     with open(f'{cp}/CUB_200_2011/train_test_split.txt') as f:
@@ -65,7 +76,7 @@ def load_cub():
         for line in f:
             x = line.split()[1]
             if x in count:
-                count[x]+=1
+                count[x] += 1
             else:
                 count[x] = 1
             if x == "0":
@@ -74,7 +85,7 @@ def load_cub():
     with open(f'{cp}/CUB_200_2011/images.txt') as u:
         test = []
         for line in u:
-            x,y  = line.split()[0],line.split()[1]
+            x, y = line.split()[0], line.split()[1]
             if x in pretest:
                 test.append(y)
 
@@ -91,15 +102,37 @@ def load_duts():
     file_paths = []
     for root, _, files in os.walk(fold):
         for file in files:
-            file_paths.append(os.path.join(root,file))
+            file_paths.append(os.path.join(root, file))
 
     fold2 = os.path.join(cp, 'DUTS-TE/DUTS-TE-Mask')
     fp2 = []
     for root, _, files in os.walk(fold2):
         for file in files:
-            fp2.append(os.path.join(root,file))
+            fp2.append(os.path.join(root, file))
 
     masks = sorted(fp2)
     test = sorted(file_paths)
 
     return test, masks
+
+
+def load_custom():
+    cp = os.path.join(os.getcwd(), 'datasets', 'CUSTOM')
+    image_dir = os.path.join(cp, 'images')
+    mask_dir = os.path.join(cp, 'masks')
+
+    image_paths = sorted([
+        os.path.join(image_dir, fname)
+        for fname in os.listdir(image_dir)
+        if fname.endswith('.png')
+    ])
+    mask_paths = sorted([
+        os.path.join(mask_dir, fname)
+        for fname in os.listdir(mask_dir)
+        if fname.endswith('.png')
+    ])
+
+    if len(image_paths) != len(mask_paths):
+        raise ValueError("Mismatch between number of images and masks in CUSTOM dataset.")
+
+    return image_paths, mask_paths
